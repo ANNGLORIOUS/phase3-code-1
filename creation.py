961c0988faa6f1db6e736ffd4be800c6575c9f53
ANNGLORIOUS <mueniannglorious@gmail.com>
2024-09-01 12:36:00 +0300

    commenta are added

--- creation.py
+++ creation.py
@@ -1,10 +1,13 @@
 class Car:
+    """The _init__ method  sets the make,model and year of the car."""
     def __init__(self, make, model, year):
+        """The self keyword refers to the instance of the car class."""
         self.make = make  
         self.model = model
         self.year = year
 
     def display_info(self):
+        """Display information about the car"""
         return f"{self.make} {self.model} ({self.year})"
 
 my_car = Car("Honda", "CRV", 2019)
