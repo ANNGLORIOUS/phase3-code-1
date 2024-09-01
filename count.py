b6827206801e953fc395f0059a02e73c4a06dba6
ANNGLORIOUS <mueniannglorious@gmail.com>
2024-09-01 10:50:06 +0300

    count and reverse file have been changed

--- count.py
+++ count.py
@@ -2,3 +2,11 @@ def count_vowels(str):
    """returns the count of vowels (a, e, i, o, u) in the string. Ignore case sensitivity """
    vowels = 'aeiouAEIOU'
    return sum(1 for char in str if char in vowels)
+
+"""test the function"""
+print(count_vowels('Hello World'))
+print(count_vowels('Python'))  
+print(count_vowels('coding challenge')) 
+print(count_vowels('AnnGlorious mueni'))  
+print(count_vowels('éxamplë'))  
+print(count_vowels(''))  
--- reverse.py
+++ reverse.py
@@ -2,4 +2,12 @@ def reverse_string(text):
     """Reverses the given string text as input and returns the reversed version of that string.
 """
     return text[::-1]
+
+# Test the function
+print(reverse_string("Hello World")) 
+print(reverse_string("Python"))       
+print(reverse_string("coding challenge"))  
+print(reverse_string("AnnGlorious mueni"))  
+print(reverse_string("paula"))         
+print(reverse_string(""))              
     
