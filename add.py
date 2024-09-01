94eee627fc9f3cf9be828161a36b44359809139a
ANNGLORIOUS <mueniannglorious@gmail.com>
2024-09-01 10:36:19 +0300

    app file modified

--- add.py
+++ add.py
@@ -1,3 +1,14 @@
+"""Function to calculate the sum of two numbers"""
 def add_numbers(num1, num2):
     """Add numbers"""
     return num1 + num2
+
+"""users input"""
+num1 = float(input("Enter first number:"))
+num2 = float(input("Enter second number:"))
+
+"""calculate sum"""
+total_sum = add_numbers(num1, num2)
+
+"""print result"""
+print(f"The sum of {num1} and {num2} is: {total_sum}")
