68aefa8657f6c559aab7152d009ea69c1a1c40e0
ANNGLORIOUS <mueniannglorious@gmail.com>
2024-09-24 08:50:59 +0300

    changed the factorial file

--- factorial.py
+++ factorial.py
@@ -1,9 +1,15 @@
 def calculate_factorial(n):
     """Calculate the factorial of a given number n"""
     factorial = 1
-    for i in range(1, n + 1):
-        factorial *= i
-    return factorial
+    if n < 0:
+        print(f"Invalid!! Enter a Positive Integer.")
+    elif n == 0 or n == 1:
+        return 1
+    else:
+        factorial = 1
+        for i in range (2, n +1):
+            factorial *= i
+        return factorial
 
 """test the function"""  
 print(calculate_factorial(5))
