b6881a24c8aca0882a26d4ecdcc29d1b2180902b
ANNGLORIOUS <mueniannglorious@gmail.com>
2024-09-24 08:55:32 +0300

    decorator corrected

--- decorator.py
+++ decorator.py
@@ -1,26 +1,12 @@
-def decorator_func(func):
-    """
-    This is a decorator function that will be applied to any function passed to it.
-    """
-    def wrapper(*args, **kwargs):
+def apply_decorator( func ):
+    def wrapper(): 
         print("Decorator Applied")
-
-        """Call the original function with any arguments and keyword arguments"""
-        return func(*args, **kwargs)
+        func() 
     return wrapper
 
-@decorator_func
-def example_function():
-    print("we have something inside")
+@apply_decorator 
+def decorator_func(): 
+    print("I am now Called!")
 
-# Call the decorated function
-example_function()
-
-# Test the decorator with a function 
-@decorator_func
-def add_numbers(a, b):
-    return a + b
-
-# Call the decorated function with arguments
-result = add_numbers(3, 5)
-print(f"The result of adding 3 and 5 is: {result}")
+# Calling the Function
+decorator_func()
